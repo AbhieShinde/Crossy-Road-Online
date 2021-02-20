c584e7e0a1114fa8655bd792facad3787ec33ea9
ashinde <shindea890@gmail.com>
2021-02-21 00:23:33 +0530

    FIX: Require Path Issue

--- index.php
+++ index.php
@@ -6,17 +6,17 @@ use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
 use Slim\Handlers\Strategies\RequestHandler;
 
-require __DIR__ . './vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 define('APP_ENV', $_ENV['APP_ENV'] ?? $_SERVER['APP_ENV'] ?? 'DEVELOPMENT');
-$settings = (require __DIR__ . './config/settings.php')(APP_ENV);
+$settings = (require __DIR__ . '/config/settings.php')(APP_ENV);
 
 // Set up dependencies
 $containerBuilder = new ContainerBuilder();
 if($settings['di_compilation_path']) {
     $containerBuilder->enableCompilation($settings['di_compilation_path']);
 }
-(require __DIR__ . './config/dependencies.php')($containerBuilder, $settings);
+(require __DIR__ . '/config/dependencies.php')($containerBuilder, $settings);
 
 // Create app
 AppFactory::setContainer($containerBuilder->build());
@@ -26,10 +26,10 @@ $app = AppFactory::create();
 $app->getRouteCollector()->setDefaultInvocationStrategy(new RequestHandler(true));
 
 // Register middleware
-(require __DIR__ . './config/middleware.php')($app);
+(require __DIR__ . '/config/middleware.php')($app);
 
 // Register routes
-(require __DIR__ . './config/routes.php')($app);
+(require __DIR__ . '/config/routes.php')($app);
 
 // Run app
 $app->run();
